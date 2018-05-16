29cd8e34281e11ca24f416906b889b774dfec47a
avn <avnermer7@gmail.com>
2018-05-16 16:59:47 +0300

    ...

---
+++ ImageService/Logging/LogCollectionSingelton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel; 
+
+namespace ImageService.Logging
+{
+    class LogCollectionSingelton
+    {
+        private static LogCollectionSingelton instance;
+        public ObservableCollection<Log> list { get; private set; }
+
+        private LogCollectionSingelton() { } 
+
+        public static LogCollectionSingelton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LogCollectionSingelton();
+                }
+                return instance;
+            }
+        }
+    }
+}
