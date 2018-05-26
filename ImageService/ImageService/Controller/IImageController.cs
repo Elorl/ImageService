dcbe9c3e93edbb70fb22bc370aa75c30eeb01c67
Elorl <elor14@gmail.com>
2018-05-26 22:33:42 +0300

    update controller

--- ImageService/ImageService/Controller/IImageController.cs
+++ ImageService/ImageService/Controller/IImageController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageService.Server;
 
 namespace ImageService.Controller
 {
     public interface IImageController
     {
+        ImageServer ImageServer { set; }
         string ExecuteCommand(int commandID, string[] args, out bool result);          // Executing the Command Requet
     }
 }
