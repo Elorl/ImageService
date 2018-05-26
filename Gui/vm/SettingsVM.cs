0d60b431b344a15fc3a245b1d3af6d8a213605c3
Elorl <elor14@gmail.com>
2018-05-26 22:50:06 +0300

    update settings VM and nuget

--- Gui/vm/SettingsVM.cs
+++ Gui/vm/SettingsVM.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Gui.models;
-
+using Prism.Commands;
+using System.Windows.Input;
+using infrastructure.Events;
+using infrastructure.Enums;
 namespace Gui.settingsVM
 {
-    class SettingsVM: ISettingsVM, INotifyPropertyChanged
+    class SettingsVM : ISettingsVM, INotifyPropertyChanged
     {
         private SettingsModel SettingsModel;
+        public ICommand RemoveCommand { get; private set; }
         #region events
         public event PropertyChangedEventHandler PropertyChanged;
         #endregion
-        
+        private string _SelectedItem;
         public SettingsVM()
         {
             this.SettingsModel = new SettingsModel();
-
+            this.SettingsModel.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) {
+                NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+            this.RemoveCommand = new DelegateCommand<object>(this.OnRemove, CanRemove);
         }
 
         public string OutputDir
@@ -34,9 +41,22 @@ namespace Gui.settingsVM
         {
             get { return this.SettingsModel.LogName; }
         }
-        public int ThumbnailSize
+        public string ThumbnailSize
         {
             get { return this.SettingsModel.ThumbnailSize; }
+        }
+        public string SelectedItem
+        {
+            get
+            {
+                return this._SelectedItem;
+            }
+            set
+            {
+                this._SelectedItem = value;
+                var command = this.RemoveCommand as DelegateCommand<object>;
+                command.RaiseCanExecuteChanged();
+            }
         }
         public ObservableCollection<string> Handlers
         {
@@ -44,13 +64,42 @@ namespace Gui.settingsVM
             {
                 return this.SettingsModel.handlers;
             }
-            set => throw new NotImplementedException();
+            set
+            {
+
+            }
         }
 
         private void NotifyPropertyChanged(string Name)
         {
             PropertyChangedEventArgs propertyChangedEventArgs = new PropertyChangedEventArgs(Name);
             this.PropertyChanged?.Invoke(this, propertyChangedEventArgs);
+        }
+
+        private void OnRemove(Object objct)
+        {
+            try
+            {
+                string[] selected = { this.SelectedItem };
+                CommandRecievedEventArgs command = new CommandRecievedEventArgs((int)CommandEnum.CloseCommand, selected, "");
+                this.SettingsModel.SendCommand(command);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private bool CanRemove(object obj)
+        {
+            if (this.SelectedItem == null || this.SelectedItem.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
